bd59cd3531674a05558c72cd1426c3e70528fbc6
visionnote <visionnote@naver.com>
2025-05-21 16:27:21 +0900

    logonDBBean,DataBean

--- Boot-Employees REST API5/src/main/java/com/example/myapp/dto/LogonDataBean.java
+++ Boot-Employees REST API5/src/main/java/com/example/myapp/dto/LogonDataBean.java
@@ -1,5 +1,42 @@
 package com.example.myapp.dto;
 
-public class LogonDataBean {
+import java.sql.Timestamp;
 
+public class LogonDataBean {
+	private String id;
+    private String passwd;
+    private String name;
+    private Timestamp reg_date;
+    private Timestamp mod_date;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPasswd() {
+		return passwd;
+	}
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Timestamp getReg_date() {
+		return reg_date;
+	}
+	public void setReg_date(Timestamp reg_date) {
+		this.reg_date = reg_date;
+	}
+	public Timestamp getMod_date() {
+		return mod_date;
+	}
+	public void setMod_date(Timestamp mod_date) {
+		this.mod_date = mod_date;
+	}
+    
 }
